3e5c6215104aba8866ba3880471a1af4807f93d8
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2021-05-03 13:01:56 -0400

    Fixing sorting order of data keys

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -51,7 +51,7 @@ var zoteroRoam = {};
                         // Records are sorted alphabetically (by key name) => _multiField should come last
                         const filteredMatches = Array.from(new Set(list.map((item) => item.index))).map((index) => {
                             return list.sort((a,b) => {
-                                return (a.key < b.key ? -1 : 1);
+                                if(a.key == "_multiField"){ return -100} else{ return (a.key < b.key ? -1 : 1) };
                             }).find(item => item.index === index);
                         });
                         return filteredMatches;
