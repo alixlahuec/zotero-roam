336342cb6ba49972091213a04e3dd9181d49cf32
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2021-06-07 21:42:54 -0400

    v0.6.0 release

--- src/init.js
+++ src/init.js
@@ -373,6 +373,7 @@ var zoteroRoam = {};
                                             .zotero-roam-tribute ul li {padding: 2px 5px;font-weight:300;}
                                             .zotero-roam-tribute-selected {background-color: #4f97d4;color:white;}
                                             .zotero-roam-page-doi{margin:10px;display:block;font-weight:600;letter-spacing:0.3mm;}
+                                            .zotero-roam-page-doi a{color:#c6c6c6;}
                                             .zotero-roam-page-menu{justify-content:space-between;border:1px #eaeaea solid;padding:10px;border-radius:5px;background-color: #f8f8f9;}
                                             .zotero-roam-page-menu-header{display:flex;}
                                             .zotero-roam-page-menu-actions{flex: 0 1 75%;flex-wrap:wrap;}
