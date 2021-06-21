6d79db1abab5e717e86d203481b879d2b2fdbe8e
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2021-06-21 11:16:28 -0400

    v0.6.25 release

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -301,7 +301,7 @@ var zoteroRoam = {};
                             "pdfLinks", "sciteBadge", "connectedPapers", "semanticScholar", "googleScholar", "citingPapers"]
                 }
             },
-            requests: {}, // Assigned the processed Array of requests (see handlers.setupUserRequests)
+            requests: [], // Assigned the processed Array of requests (see handlers.setupUserRequests)
             shortcuts: [], // Assigned the processed Array of zoteroRoam.Shortcut objects (see shortcuts.setup)
             userSettings: {}, // Assigned the value of the zoteroRoam_settings Object defined by the user (see run.js)
             ref_checking: null,
