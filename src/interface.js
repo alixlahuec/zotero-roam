adb97b2cfddcf42c9598835ddca65bb28b202555
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2021-07-23 19:59:59 -0400

    Fixing context menu action target

--- src/interface.js
+++ src/interface.js
@@ -672,8 +672,8 @@
 
             elementsKeys.forEach(key => {
                 zoteroRoam.interface[`${key}`].div.addEventListener("click", (e) => {
-                    let target = e.target;
-                    let op = target.closest('li.zotero-roam-cm-option');
+                    let target = zoteroRoam.interface.contextTarget;
+                    let op = e.target.closest('li.zotero-roam-cm-option');
                     if(op){
                         let action = op.innerText;
                         switch(action){
