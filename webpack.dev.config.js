b719ebacc8001a492452571f91b96f3f39a426e6
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-02-19 11:16:59 -0500

    Set non-minified dev environment

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.config");
 
 module.exports = merge(baseConfig, {
+	mode: "development",
 	optimization: {
 		minimize: false
 	},
