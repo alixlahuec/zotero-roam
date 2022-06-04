3375299749cf366973dd134f59bd9b2e324d134a
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-04 17:28:08 -0400

    feat: add DataDrawer component

---
+++ src/components/DataDrawer/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Classes, Drawer } from "@blueprintjs/core";
+import { bool, func, object } from "prop-types";
+
+function RawItem({ item }){
+	return <pre className={Classes.CODE_BLOCK}>{JSON.stringify(item, null, "\t")}</pre>;
+}
+RawItem.propTypes = {
+	item: object
+};
+
+function DataDrawer({ item, isOpen, onClose }){
+	return (
+		<Drawer
+			canEscapeKeyClose={false}
+			canOutsideClickClose={true}
+			className="zr-drawer--data"
+			isOpen={isOpen}
+			lazy={false}
+			onClose={onClose}
+			size="40%" >
+			<RawItem item={item} />
+		</Drawer>
+	);
+}
+DataDrawer.propTypes = {
+	item: object,
+	isOpen: bool,
+	onClose: func
+};
+
+export default DataDrawer;
