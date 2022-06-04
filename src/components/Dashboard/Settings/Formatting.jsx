38e03b14ceb13f90524476519bf261c5f6945534
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-03 21:21:54 -0400

    feat: add annotations to Settings

--- src/components/Dashboard/Settings/Formatting.jsx
+++ src/components/Dashboard/Settings/Formatting.jsx
@@ -4,7 +4,7 @@ import { Classes, H3, H4 } from "@blueprintjs/core";
 import { UserSettings } from "../../App";
 
 function Formatting(){
-	const { metadata, notes } = useContext(UserSettings);
+	const { annotations, metadata, notes } = useContext(UserSettings);
 
 	return (
 		<>
@@ -12,8 +12,16 @@ function Formatting(){
 			<div zr-role="settings-formatting">
 				<H4>Metadata</H4>
 				<pre className={Classes.CODE_BLOCK}>{JSON.stringify(metadata, null, "\t")}</pre>
+				{metadata.use == "function" && metadata.func
+					? <pre className={Classes.CODE_BLOCK}>{window[metadata.func].toString()}</pre>
+					: null}
 				<H4>Notes</H4>
 				<pre className={Classes.CODE_BLOCK}>{JSON.stringify(notes, null, "\t")}</pre>
+				{notes.func
+					? <pre className={Classes.CODE_BLOCK}>{window[notes.func].toString()}</pre>
+					: null}
+				<H4>Annotations</H4>
+				<pre className={Classes.CODE_BLOCK}>{JSON.stringify(annotations, null, "\t")}</pre>
 			</div>
 		</>
 	);
