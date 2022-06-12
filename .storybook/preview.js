5f1ae10b4b259f05c65b8081028af9c5af8eea6c
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-12 18:51:56 -0400

    infra: add CSS global imports to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+import "../node_modules/@blueprintjs/select/lib/css/blueprint-select.css";
+import "../node_modules/@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import "../node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css";
+import "../src/index.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
