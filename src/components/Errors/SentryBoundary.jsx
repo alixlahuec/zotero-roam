732c3cc8c3c68d61299c24b38c6363de76cc6dbb
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-11 22:57:29 -0400

    fix: fatal error in SentryBoundary [ZOTERO-ROAM-3]

--- src/components/Errors/SentryBoundary.jsx
+++ src/components/Errors/SentryBoundary.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import * as Sentry from "@sentry/react";
 import { node } from "prop-types";
+
 import ErrorCallout from "./ErrorCallout";
 
-const fallback = ({ error, resetError }) => {
-	return <ErrorCallout buttonText="Reload" error={error} resetErrorBoundary={resetError} />;
-};
-
 function SentryBoundary({ children }){
-
-	return <Sentry.Boundary fallback={(props) => fallback(props)} >
+	return <Sentry.Boundary fallback={({ error, resetError }) => (
+		<ErrorCallout buttonText="Reload" error={error} resetErrorBoundary={resetError} />
+	)}>
 		{children}
 	</Sentry.Boundary>;
 }
