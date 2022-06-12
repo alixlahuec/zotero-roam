66d2cd161db5e16de52b091ec8b6564a73125e58
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-12 12:44:15 -0400

    infra: set tags, extra data in SentryBoundary

--- src/components/Errors/SentryBoundary.jsx
+++ src/components/Errors/SentryBoundary.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { ErrorBoundary } from "@sentry/react";
-import { node } from "prop-types";
+import { array, node, object, oneOfType, string } from "prop-types";
 
 import ErrorCallout from "./ErrorCallout";
 
 function SentryBoundary(props){
-	return <ErrorBoundary fallback={({ error, resetError }) => (
-		<ErrorCallout buttonText="Reload" error={error} resetErrorBoundary={resetError} />
-	)}>
+	const { feature = null, extra = null } = props;
+	return <ErrorBoundary 
+		beforeCapture={(scope, _error, _componentStack) => {
+			if(feature){
+				scope.setTag("feature", feature);
+			}
+			if(extra){
+				scope.setExtras({...extra});
+			}
+		}}
+		fallback={({ error, resetError }) => (
+			<ErrorCallout buttonText="Reload" error={error} resetErrorBoundary={resetError} />
+		)}>
 		{props.children}
 	</ErrorBoundary>;
 }
 SentryBoundary.propTypes = {
-	children: node
+	children: node,
+	extra: oneOfType([array, object]),
+	feature: string,
 };
 
 export default SentryBoundary;
