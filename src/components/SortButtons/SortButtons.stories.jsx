40e9a877c8c35450a21e2454650f67f7304a8547
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-12 18:52:56 -0400

    infra: add story for SortButtons
    * fixes small visual glitch on focus

---
+++ src/components/SortButtons/SortButtons.stories.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import SortButtons from ".";
+
+export default { 
+	component: SortButtons,
+	args: {
+		name: "item-sort",
+		onSelect: () => {}
+	}
+};
+
+const Template = (args) => <SortButtons {...args} />;
+
+export const WithIcons = Template.bind({});
+WithIcons.args = {
+	options: [
+		{icon: "eye-open", label: "Visibility", value: "visibility"},
+		{icon: "history", label: "Recency", value: "recency"},
+		{icon: "cloud", label: "Location", value: "location"}
+	],
+	selectedOption: "visibility"
+};
+
+export const WithoutIcons = Template.bind({});
+WithoutIcons.args = {
+	options: [
+		{label: "Alphabetical (A-Z)", value: "alphabetical"},
+		{label: "Publication Year", value: "published"},
+		{label: "Priority", value: "priority"}
+	],
+	selectedOption: "priority"
+};
