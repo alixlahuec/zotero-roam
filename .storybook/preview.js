66bc42d9f288a49fc8ff9cec96b8971c77038836
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-12 18:52:15 -0400

    infra: add global for dark theme in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,32 @@ import "../node_modules/@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 import "../node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 import "../src/index.css";
 
+// https://storybook.js.org/docs/react/essentials/toolbars-and-globals
+const withTheme = (Story, context) => {
+  const isDark = context.globals.theme == "dark";
+  return <div zr-dark-theme={isDark.toString()} 
+    style={{ backgroundColor: "var(--zr-dialog-bg)", margin: "50px", padding: "20px", height: "1000px" }}>
+    <Story {...context} />
+  </div>;
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+      // Change title based on selected value
+      dynamicTitle: true,
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -14,3 +40,4 @@ export const parameters = {
   },
 }
 
+export const decorators = [withTheme];
