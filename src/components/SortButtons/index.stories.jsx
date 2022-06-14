2ecdb197c011debcacaad37dc6b873c9102da396
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-14 19:32:09 -0400

    feat: add template decorator to mock controlled component

--- src/components/SortButtons/index.stories.jsx
+++ src/components/SortButtons/index.stories.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import SortButtons from ".";
 
 export default { 
 	component: SortButtons,
 	args: {
-		name: "item-sort",
-		onSelect: () => {}
+		name: "item-sort"
 	}
 };
 
-const Template = (args) => <SortButtons {...args} />;
+const Template = (args) => {
+	const [selected, setSelected] = useState("published");
+	const onSelect = useCallback((val) => setSelected(val), []);
+
+	return <SortButtons onSelect={onSelect} selectedOption={selected} {...args} />;
+};
 
 export const WithIcons = Template.bind({});
 WithIcons.args = {
 	options: [
 		{icon: "eye-open", label: "Visibility", value: "visibility"},
-		{icon: "history", label: "Recency", value: "recency"},
+		{icon: "history", label: "Publication Year", value: "published"},
 		{icon: "cloud", label: "Location", value: "location"}
-	],
-	selectedOption: "visibility"
+	]
 };
 
 export const WithoutIcons = Template.bind({});
@@ -27,6 +30,5 @@ WithoutIcons.args = {
 		{label: "Alphabetical (A-Z)", value: "alphabetical"},
 		{label: "Publication Year", value: "published"},
 		{label: "Priority", value: "priority"}
-	],
-	selectedOption: "priority"
+	]
 };
