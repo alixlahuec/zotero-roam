6a6106fc3f2bb2101e4e45aa0aaea1d789371601
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-18 21:06:12 -0400

    fix: mock data for libraries

--- mocks/zotero/libraries.js
+++ mocks/zotero/libraries.js
@@ -1,14 +1,18 @@
-const configLibrary = (type, id, name) => ({ 
-	id,
-	links: {
-		alternate: {
-			href: "https://www.zotero.org/" + name,
-			type: "text/html"
-		}
-	},
-	path: type + "s/" + id,
-	type
-});
+const configLibrary = (type, id, name) => {
+	const path = type + "s/" + id;
+	return {
+		id,
+		links: {
+			alternate: {
+				href: "https://www.zotero.org/" + (type == "user" ? name : path),
+				type: "text/html"
+			}
+		},
+		name,
+		path,
+		type
+	};
+};
 
 export const data = {
 	"userLibrary": {
