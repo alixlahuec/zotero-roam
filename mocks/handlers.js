fb50849a7c849a3fc9b7f850bd1e0dc2216fc8b6
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-18 21:07:37 -0400

    chore: minor fixes

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -5,11 +5,16 @@ import { handleAPIKey } from "./zotero/keys";
 export const handlers = [
 	...handleCollections,
 	handleAPIKey,
+	// Catch requests for Roam CSS assets
+	rest.get("https://roamresearch.com/assets/css/*", (req, res, ctx) => {
+		return res(
+			ctx.status(301, "Ignoring a request for Roam CSS resources at " + req.url)
+		);
+	}),
 	// Fallback
 	rest.get("*", (req, res, ctx) => {
 		return res(
-			ctx.status(404, "You need to add a handler for " + req.url),
-			ctx.json({})
+			ctx.status(404, "You need to add a handler for " + req.url)
 		);
 	})
 ];
