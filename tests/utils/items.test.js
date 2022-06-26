be9013587ed51162f5ce78501361972cf08c124a
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-06-26 16:58:03 -0400

    chore: use mock data for metadata cleaning tests

--- tests/utils/items.test.js
+++ tests/utils/items.test.js
@@ -5,6 +5,11 @@ import {
 	cleanLibrary, 
 	cleanSemantic, 
 	identifyChildren } from "../../src/utils";
+import { semantics } from "../../mocks/semantic-scholar";
+import { items } from "../../mocks/zotero/items";
+import { libraries } from "../../mocks/zotero/libraries";
+
+const { userLibrary } = libraries;
 
 test("Categorizes Zotero items", () => {
 	const items = [
@@ -73,244 +78,131 @@ describe("Formatting authorship data", () => {
 });
 
 test("Simplifies Zotero item metadata", () => {
-	const item = {
-		"key": "X42A7DEE",
-		"version": 1,
-		"library": {
-			"type": "user",
-			"id": 475425,
-			"name": "Z public library",
-			"links": {
-				"alternate": {
-					"href": "https://www.zotero.org/z_public_library",
-					"type": "text/html"
-				}
-			}
-		},
-		"links": {
-			"self": {
-				"href": "https://api.zotero.org/users/475425/items/X42A7DEE",
-				"type": "application/json"
-			},
-			"alternate": {
-				"href": "https://www.zotero.org/z_public_library/items/X42A7DEE",
-				"type": "text/html"
-			}
-		},
-		"meta": {
-			"creatorSummary": "Institute of Physics (Great Britain)",
-			"parsedDate": "1993-00-00",
-			"numChildren": 0
-		},
-		"data": {
-			"key": "X42A7DEE",
-			"version": 1,
-			"itemType": "book",
-			"title": "Electron Microscopy and Analysis 1993: Proceedings of the Institute of Physics Electron Microscopy and Analysis Group Conference, University of Liverpool, 14-17 September1993",
-			"creators": [
-				{
-					"creatorType": "author",
-					"name": "Institute of Physics (Great Britain)"
-				},
-				{
-					"creatorType": "contributor",
-					"firstName": "A. J",
-					"lastName": "Craven"
-				},
-				{
-					"creatorType": "contributor",
-					"name": "Institute of Materials (Great Britain)"
-				},
-				{
-					"creatorType": "contributor",
-					"name": "Royal Microscopical Society (Great Britain)"
-				},
-				{
-					"creatorType": "contributor",
-					"name": "University of Liverpool"
-				}
-			],
-			"abstractNote": "",
-			"series": "Institute of Physics conference series",
-			"seriesNumber": "no. 138",
-			"volume": "",
-			"numberOfVolumes": "",
-			"edition": "",
-			"place": "Bristol, UK",
-			"publisher": "Institute of Physics Pub",
-			"date": "1993",
-			"numPages": "546",
-			"language": "",
-			"ISBN": "0750303212",
-			"shortTitle": "Electron Microscopy and Analysis 1993",
-			"url": "",
-			"accessDate": "",
-			"archive": "",
-			"archiveLocation": "",
-			"libraryCatalog": "cat.cisti-icist.nrc-cnrc.gc.ca Library Catalog",
-			"callNumber": "QC1 I584 v. 138",
-			"rights": "",
-			"extra": "",
-			"dateAdded": "2011-01-13T03:37:29Z",
-			"dateModified": "2011-01-13T03:37:29Z",
-			"tags": [
-				{
-					"tag": "Analysis",
-					"type": 1
-				},
-				{
-					"tag": "Congresses",
-					"type": 1
-				},
-				{
-					"tag": "Electron microscopy",
-					"type": 1
-				},
-				{
-					"tag": "Materials",
-					"type": 1
-				},
-				{
-					"tag": "Microscopy",
-					"type": 1
-				}
-			],
-			"collections": [
-				"BX9965IJ",
-				"9KH9TNSJ"
-			],
-			"relations": {
-				"owl:sameAs": "http://zotero.org/groups/36222/items/E6IGUT5Z"
-			}
-		}
-	};
-
-	expect(cleanLibrary([item], new Map()))
+	expect(cleanLibrary([items[0]], new Map()))
 		.toEqual([{
-			abstract: "",
-			authors: "Institute of Physics (Great Britain)",
-			authorsFull: ["Institute of Physics (Great Britain)", "A. J Craven", "Institute of Materials (Great Britain)", "Royal Microscopical Society (Great Britain)", "University of Liverpool"],
-			authorsLastNames: ["Institute of Physics (Great Britain)", "Craven", "Institute of Materials (Great Britain)", "Royal Microscopical Society (Great Britain)", "University of Liverpool"],
-			authorsRoles: ["author", "contributor", "contributor", "contributor", "contributor"],
+			abstract: items[0].data.abstractNote,
+			authors: "Bloch and Rozmovits",
+			authorsFull: ["Gary Bloch", "Linda Rozmovits"],
+			authorsLastNames: ["Bloch", "Rozmovits"],
+			authorsRoles: ["author", "author"],
 			children: {
 				pdfs: [],
 				notes: []
 			},
 			createdByUser: null,
 			inGraph: false,
-			itemKey: "X42A7DEE",
-			itemType: "book",
-			key: "X42A7DEE",
-			location: "users/475425",
-			meta: "Institute of Physics (Great Britain)",
-			publication: "",
-			tags: ["Analysis", "Congresses", "Electron microscopy", "Materials", "Microscopy"],
-			title: "Electron Microscopy and Analysis 1993: Proceedings of the Institute of Physics Electron Microscopy and Analysis Group Conference, University of Liverpool, 14-17 September1993",
-			weblink: false,
-			year: "",
-			zotero: {
-				local: "zotero://select/library/items/X42A7DEE",
-				web: "https://www.zotero.org/users/475425/items/X42A7DEE"
+			itemKey: items[0].data.key,
+			itemType: items[0].data.itemType,
+			key: "blochImplementingSocialInterventions2021",
+			location: userLibrary.path,
+			meta: "Bloch and Rozmovits (2021)",
+			publication: "CMAJ",
+			tags: [],
+			title: "Implementing social interventions in primary care",
+			weblink: {
+				href: "https://www.cmaj.ca/content/193/44/E1696",
+				title: "https://www.cmaj.ca/content/193/44/E1696"
 			},
-			raw: item,
-			_multiField: "Institute of Physics (Great Britain) A. J Craven Institute of Materials (Great Britain) Royal Microscopical Society (Great Britain) University of Liverpool Electron Microscopy and Analysis 1993: Proceedings of the Institute of Physics Electron Microscopy and Analysis Group Conference, University of Liverpool, 14-17 September1993 #Analysis, #Congresses, #Electron microscopy, #Materials, #Microscopy X42A7DEE"
+			year: "2021",
+			zotero: {
+				local: "zotero://select/library/items/" + items[0].data.key,
+				web: "https://www.zotero.org/" + userLibrary.path + "/items/" + items[0].data.key
+			},
+			raw: items[0],
+			_multiField: items[0].data.abstractNote + " Gary Bloch Linda Rozmovits 2021 Implementing social interventions in primary care blochImplementingSocialInterventions2021"
 		}]);
 });
 
 test("Simplifies SemanticScholar item metadata", () => {
-	const semantic = {
-		citations: [
-			{
-				arxivId: null,
-				authors: [
-					{authorId: "1745379", name: "Kelsey S. Dickson"},
-					{authorId: "8203031", name: "M. Sklar"},
-					{authorId: "2136449585", name: "Serena Chen"},
-					{authorId: "83992086", name: "Bo-yeong Kim"}
-				],
-				doi: "10.1186/s12913-022-07748-2",
-				intent: [],
-				isInfluential: false,
-				paperId: "05f634ed8569c49f347870397a4791425c238267",
-				title: "Characterization of multilevel influences of mental health care transitions: a comparative case study analysis",
-				url: "https://www.semanticscholar.org/paper/05f634ed8569c49f347870397a4791425c238267",
-				venue: "BMC Health Services Research",
-				year: 2022
-			}
-		],
-		references: [
-			{
-				arxivId: null,
-				authors: [
-					{authorId: "3029609", name: "J. Ormel"},
-					{authorId: "2556648", name: "A. Oerlemans"},
-					{authorId: "144166950", name: "D. Raven"},
-					{authorId: "5394184", name: "O. Laceulle"},
-					{authorId: "38686825", name: "C. Hartman"},
-					{authorId: "2890242", name: "R. Veenstra"},
-					{authorId: "3058144", name: "F. Verhulst"},
-					{authorId: "4722145", name: "W. Vollebergh"},
-					{authorId: "5645154", name: "J. Rosmalen"},
-					{authorId: "3900129", name: "S. Reijneveld"},
-					{authorId: "145408508", name: "A. Oldehinkel"}
-				],
-				doi: "10.1017/S0033291716003445",
-				intent: [],
-				isInfluential: false,
-				paperId: "00723c630a38ef43228dd0eb9a8b357ee76445b6",
-				title: "Functional outcomes of child and adolescent mental disorders. Current disorder most important but psychiatric history matters as well",
-				url: "https://www.semanticscholar.org/paper/00723c630a38ef43228dd0eb9a8b357ee76445b6",
-				venue: "Psychological Medicine",
-				year: 2017
-			}
-		]
-	};
-	expect(cleanSemantic([], semantic, new Map()))
+	const { citations, references } = Object.values(semantics)[0];
+	expect(cleanSemantic([], { citations, references }, new Map()))
 		.toEqual({
 			citations: [
-				{   
-					authors: "Dickson et al.",
-					authorsLastNames: ["Dickson", "Sklar", "Chen", "Kim"],
-					authorsString: "Dickson Sklar Chen Kim",
-					doi: "10.1186/s12913-022-07748-2",
-					intent: [],
-					isInfluential: false,
-					inGraph: false, 
-					inLibrary: false,
-					links: {
-						"semantic-scholar": "https://www.semanticscholar.org/paper/05f634ed8569c49f347870397a4791425c238267",
-						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.1186/s12913-022-07748-2",
-						"google-scholar": "https://scholar.google.com/scholar?q=10.1186/s12913-022-07748-2"
-					},
-					meta: "BMC Health Services Research",
-					title: "Characterization of multilevel influences of mental health care transitions: a comparative case study analysis",
-					url: "https://www.semanticscholar.org/paper/05f634ed8569c49f347870397a4791425c238267",
-					year: "2022",
-					_type: "citing",
-					_multiField: "Dickson Sklar Chen Kim 2022 Characterization of multilevel influences of mental health care transitions: a comparative case study analysis"
-				}
-			],
-			references: [
 				{
-					authors: "Ormel et al.",
-					authorsLastNames: ["Ormel", "Oerlemans", "Raven", "Laceulle", "Hartman", "Veenstra", "Verhulst", "Vollebergh", "Rosmalen", "Reijneveld", "Oldehinkel"],
-					authorsString: "Ormel Oerlemans Raven Laceulle Hartman Veenstra Verhulst Vollebergh Rosmalen Reijneveld Oldehinkel",
-					doi: "10.1017/s0033291716003445",
+					authors: "Knox et al.",
+					authorsLastNames: ["Knox", "Esteban", "Hernandez", "Fleming", "Safaeinilli", "Brewster"],
+					authorsString: "Knox Esteban Hernandez Fleming Safaeinilli Brewster",
+					doi: "10.1136/bmjoq-2021-001807",
 					intent: [],
 					isInfluential: false,
 					inGraph: false,
 					inLibrary: false,
 					links: {
-						"semantic-scholar": "https://www.semanticscholar.org/paper/00723c630a38ef43228dd0eb9a8b357ee76445b6",
-						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.1017/S0033291716003445",
-						"google-scholar": "https://scholar.google.com/scholar?q=10.1017/S0033291716003445"
+						"semantic-scholar": "https://www.semanticscholar.org/paper/c3a1a1c6875a03b4d32fd0a36f54840aa6ba4fb7",
+						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.1136/bmjoq-2021-001807",
+						"google-scholar": "https://scholar.google.com/scholar?q=10.1136/bmjoq-2021-001807"
 					},
-					meta: "Psychological Medicine",
-					title: "Functional outcomes of child and adolescent mental disorders. Current disorder most important but psychiatric history matters as well",
-					url: "https://www.semanticscholar.org/paper/00723c630a38ef43228dd0eb9a8b357ee76445b6",
+					meta: "BMJ open quality",
+					title: "Defining case management success: a qualitative study of case manager perspectives from a large-scale health and social needs support program",
+					url: "https://www.semanticscholar.org/paper/c3a1a1c6875a03b4d32fd0a36f54840aa6ba4fb7",
+					year: "2022",
+					_type: "citing",
+					_multiField: "Knox Esteban Hernandez Fleming Safaeinilli Brewster 2022 Defining case management success: a qualitative study of case manager perspectives from a large-scale health and social needs support program"
+				}
+			],
+			references: [
+				{
+					authors: "Solberg",
+					authorsLastNames: ["Solberg"],
+					authorsString: "Solberg",
+					doi: "10.1370/afm.1918",
+					intent: [],
+					isInfluential: false,
+					inGraph: false,
+					inLibrary: false,
+					links: {
+						"semantic-scholar": "https://www.semanticscholar.org/paper/447a7aa39d71dda859564dc12326a43b66488c76",
+						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.1370/afm.1918",
+						"google-scholar": "https://scholar.google.com/scholar?q=10.1370/afm.1918"
+					},
+					meta: "The Annals of Family Medicine",
+					title: "Theory vs Practice: Should Primary Care Practice Take on Social Determinants of Health Now? No.",
+					url: "https://www.semanticscholar.org/paper/447a7aa39d71dda859564dc12326a43b66488c76",
+					year: "2016",
+					_type: "cited",
+					_multiField: "Solberg 2016 Theory vs Practice: Should Primary Care Practice Take on Social Determinants of Health Now? No."
+				},
+				{
+					authors: "Fagnan",
+					authorsLastNames: ["Fagnan"],
+					authorsString: "Fagnan",
+					doi: "10.3122/jabfm.2017.01.160355",
+					intent: [],
+					isInfluential: false,
+					inGraph: false,
+					inLibrary: false,
+					links: {
+						"semantic-scholar": "https://www.semanticscholar.org/paper/da834ae1504204c6e14e4bcf8811c4ca250d4ada",
+						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.3122/jabfm.2017.01.160355",
+						"google-scholar": "https://scholar.google.com/scholar?q=10.3122/jabfm.2017.01.160355"
+					},
+					meta: "The Journal of the American Board of Family Medicine",
+					title: "Moving Upstream—Health Extension and Primary Care",
+					url: "https://www.semanticscholar.org/paper/da834ae1504204c6e14e4bcf8811c4ca250d4ada",
 					year: "2017",
 					_type: "cited",
-					_multiField: "Ormel Oerlemans Raven Laceulle Hartman Veenstra Verhulst Vollebergh Rosmalen Reijneveld Oldehinkel 2017 Functional outcomes of child and adolescent mental disorders. Current disorder most important but psychiatric history matters as well"
+					_multiField: "Fagnan 2017 Moving Upstream—Health Extension and Primary Care"
+				},
+				{
+					authors: "Andermann",
+					authorsLastNames: ["Andermann"],
+					authorsString: "Andermann",
+					doi: "10.1186/s40985-018-0094-7",
+					intent: [],
+					isInfluential: false,
+					inGraph: false,
+					inLibrary: false,
+					links: {
+						"semantic-scholar": "https://www.semanticscholar.org/paper/f090bb04877612278bd614b951a8d753eb6b8b5b",
+						"connected-papers": "https://www.connectedpapers.com/api/redirect/doi/10.1186/s40985-018-0094-7",
+						"google-scholar": "https://scholar.google.com/scholar?q=10.1186/s40985-018-0094-7"
+					},
+					meta: "Public Health Reviews",
+					title: "Screening for social determinants of health in clinical care: moving from the margins to the mainstream",
+					url: "https://www.semanticscholar.org/paper/f090bb04877612278bd614b951a8d753eb6b8b5b",
+					year: "2018",
+					_type: "cited",
+					_multiField: "Andermann 2018 Screening for social determinants of health in clinical care: moving from the margins to the mainstream"
 				}
 			],
 			backlinks: []
