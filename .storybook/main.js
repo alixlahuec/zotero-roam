a7885035e67bcf4daaa0b43e58acef49cb1a7514
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-01 19:31:14 -0400

    infra: add Storybook test runner
    * downgrade to Jest@27 due to current incompatibility with @28 (see https://github.com/storybookjs/test-runner/issues/99)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-a11y", "@storybook/addon-controls", "@storybook/addon-coverage", "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   "framework": "@storybook/react",
   core: {
