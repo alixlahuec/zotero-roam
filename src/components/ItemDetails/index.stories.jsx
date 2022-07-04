3d8e0b0a55d5f7c28e5b16348f635b7609cf6ce3
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-04 00:18:52 -0400

    feat: add interactions testing to ItemDetails story

--- src/components/ItemDetails/index.stories.jsx
+++ src/components/ItemDetails/index.stories.jsx
@@ -1,6 +1,9 @@
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { expect, jest } from "@storybook/jest";
 import React from "react";
 import ItemDetails from ".";
 
+import zrToaster from "../ExtensionToaster";
 import { items } from "../../../mocks/zotero/items";
 import { cleanLibraryItem } from "../../utils";
 
@@ -19,13 +22,13 @@ export default {
 			metadata: {},
 			notes: {},
 			shortcuts: {
-				"copyDefault": false,
-				"copyCitation": false,
-				"copyCitekey": false,
-				"copyPageRef": false,
-				"copyTag": false,
-				"goToItemPage": false,
-				"importMetadata": false,
+				"copyDefault": "alt+D",
+				"copyCitation": "alt+C+T",
+				"copyCitekey": "alt+C+K",
+				"copyPageRef": "alt+P",
+				"copyTag": "alt+T",
+				"goToItemPage": "alt+G",
+				"importMetadata": "alt+M",
 				"toggleNotes": "alt+N",
 			},
 			typemap: {}
@@ -36,3 +39,37 @@ export default {
 const Template = (args) => <ItemDetails {...args} />;
 
 export const Default = Template.bind({});
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async({ canvasElement }) => {
+	const canvas = within(canvasElement);
+	const showToasterFn = jest.spyOn(zrToaster, "show");
+	const copyMenu = canvas.getByText("Copy reference");
+
+	await userEvent.hover(copyMenu);
+
+	await userEvent.keyboard("{Alt>}D{/Alt}");
+
+	await waitFor(() => expect(showToasterFn)
+		.toHaveBeenCalled());
+    
+	await userEvent.keyboard("{Alt>}C>T/{/Alt}");
+
+	await waitFor(() => expect(showToasterFn)
+		.toHaveBeenCalled());
+    
+	await userEvent.keyboard("{Alt>}C>K{/Alt}");
+
+	await waitFor(() => expect(showToasterFn)
+		.toHaveBeenCalled());
+
+	await userEvent.keyboard("{Alt>}P{/Alt}");
+
+	await waitFor(() => expect(showToasterFn)
+		.toHaveBeenCalled());
+
+	await userEvent.keyboard("{Alt>}T{/Alt}");
+
+	await waitFor(() => expect(showToasterFn)
+		.toHaveBeenCalled());
+};
