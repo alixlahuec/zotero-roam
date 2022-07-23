ddb3a27355e529858ebc6c0301ff57e077dbcf2d
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-23 17:30:51 -0400

    feat: add interactive story for Dashboard tabs

--- src/components/Dashboard/Dashboard.stories.jsx
+++ src/components/Dashboard/Dashboard.stories.jsx
@@ -1,3 +1,5 @@
+import { expect } from "@storybook/jest";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
 import React from "react";
 import Dashboard from ".";
 
@@ -27,3 +29,39 @@ export default {
 const Template = (args) => <Dashboard {...args} />;
 
 export const Default = Template.bind({});
+
+export const VisitTabs = Template.bind({});
+VisitTabs.play = async ({ canvasElement }) => {
+	const canvas = within(canvasElement);
+	await userEvent.click(canvas.getByRole("tab", { name: "Tag Manager" }));
+
+	await waitFor(() => expect(
+		canvas.getByRole(
+			"tab",
+			{
+				name: "Suggestions"
+			}
+		)
+	).toBeInTheDocument(),
+	{
+		timeout: 3000
+	});
+
+	await userEvent.click(canvas.getByRole("tab", { name: "Explorer" }));
+
+	await waitFor(() => expect(
+		canvas.getByRole(
+			"button",
+			{
+				name: "Set filter"
+			}
+		)
+	).toBeInTheDocument(),
+	{
+		timeout: 3000
+	});
+
+	await userEvent.click(canvas.getByRole("tab", { name: "PDFs" }));
+	await userEvent.click(canvas.getByRole("tab", { name: "Notes" }));
+
+};
