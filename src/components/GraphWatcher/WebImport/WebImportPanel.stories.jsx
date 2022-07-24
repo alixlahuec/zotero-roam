32269d7b6edaa62aa61cf2f812593a797cac1a6d
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-24 18:22:36 -0400

    feat: add stories to cover api/write functions
    * add WebImportPanel interactions to cover useImportCitoids
    * add MergeAsOptions story with interactions to cover component and useModifyTags

--- src/components/GraphWatcher/WebImport/WebImportPanel.stories.jsx
+++ src/components/GraphWatcher/WebImport/WebImportPanel.stories.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import WebImportPanel from "./WebImportPanel";
-import { badIdentifier, goodIdentifier } from "Mocks/citoid";
+
+import { badIdentifier, goodIdentifier, citoids } from "Mocks/citoid";
+import { libraries } from "Mocks/zotero/libraries";
+
+import { expect, jest } from "@storybook/jest";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+
+const { userLibrary } = libraries;
 
 export default {
 	component: WebImportPanel,
@@ -28,4 +35,56 @@ WithInvalidLink.args = {
 export const WithMixedLinks = Template.bind({});
 WithMixedLinks.args = {
 	urls: [badIdentifier, goodIdentifier]
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.args = {
+	urls: [goodIdentifier]
+};
+WithInteractions.play = async({ args, canvasElement }) => {
+	const items = args.urls.map(id => citoids[id]);
+
+	document.dispatchEvent = jest.fn();
+	const expectedEvent = new CustomEvent("zotero-roam:write", {
+		bubbles: true,
+		cancelable: true,
+		detail: {
+			collections: [],
+			data: {
+				successful: [],
+				failed: []
+			},
+			error: null,
+			items,
+			library: userLibrary.path,
+			tags: []
+		}
+	});
+
+	const canvas = within(canvasElement);
+
+	await waitFor(() => expect(
+		canvas.getByRole("listitem", { name: args.urls[0] })
+	).toBeInTheDocument(),
+	{
+		timeout: 3000
+	});
+
+	const firstCitoid = await canvas.findByRole("listitem", { name: args.urls[0], queryFallbacks: true });
+	const citoidCheckbox = within(firstCitoid).getByRole("checkbox").nextSibling;
+
+	await userEvent.click(citoidCheckbox);
+
+	const importButton = canvas.getByRole("button", { name: "Send to Zotero" });
+
+	await userEvent.click(importButton);
+
+	await waitFor(() => expect(
+		document.dispatchEvent
+	).toHaveBeenCalled(), 
+	{ 
+		timeout: 3000 
+	});
+
+	await expect(document.dispatchEvent).toHaveBeenCalledWith(expectedEvent);
 };
