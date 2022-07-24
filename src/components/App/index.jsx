6a5ef1f909f462ab53ec245d5d010d4f26236d07
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-23 20:56:55 -0400

    fix: typo in package name

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 import React, { Component } from "react";
-import { QueryClient, QueryClientProvider } from "tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HotkeysTarget2 } from "@blueprintjs/core";
 
 import ExtensionIcon from "../ExtensionIcon";
