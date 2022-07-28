bf43f9c21aaa3133d72aaf9c4ec9b6ecf6e715ff
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-27 21:27:04 -0400

    chore: reformat Storybook main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)", "../stories/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-a11y", "@storybook/addon-controls", "@storybook/addon-coverage", "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": "@storybook/react",
-  features: {
-    interactionsDebugger: true,
-  },
-  core: {
-    builder: "webpack5"
-  },
-  staticDirs: ["../public"],
-  webpackFinal: async(config) => {
-      return {
-          ...config,
-          resolve: {
-              ...config.resolve,
-              alias: {
-                  "Mocks": path.resolve(__dirname, "..", "mocks"),
-                  "Roam": require.resolve("../mocks/roam.js")
-              }
-          }
-      }
-  },
+    "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)", "../stories/*.stories.@(js|jsx|ts|tsx)"],
+    "addons": ["@storybook/addon-a11y", "@storybook/addon-controls", "@storybook/addon-coverage", "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    "framework": "@storybook/react",
+    features: {
+        interactionsDebugger: true,
+    },
+    core: {
+        builder: "webpack5"
+    },
+    staticDirs: ["../public"],
+    webpackFinal: async (config) => {
+        return {
+            ...config,
+            resolve: {
+                ...config.resolve,
+                alias: {
+                    "Mocks": path.resolve(__dirname, "..", "mocks"),
+                    "Roam": require.resolve("../mocks/roam.js")
+                }
+            }
+        }
+    },
 };
