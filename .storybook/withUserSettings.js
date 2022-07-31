7f0e56ba012601c0dbdabeff61ca534eef23751b
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-07-30 22:34:39 -0400

    chore: update settings context [Storybook]

--- .storybook/withUserSettings.js
+++ .storybook/withUserSettings.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { UserSettings } from "../src/components/App";
+import { UserSettingsProvider} from "../src/components/UserSettings";
+
+import { apiKeys } from "Mocks/zotero/keys";
+import { libraries } from "Mocks/zotero/libraries";
+
+import { analyzeUserRequests, setupInitialSettings } from "../src/setup";
+
+const { keyWithFullAccess: { key: masterKey } } = apiKeys;
+const { userLibrary: { path: userPath }, groupLibrary: { path: groupPath} } = libraries;
+
+const defaultReqs = [
+    { dataURI: userPath + "/items", apikey: masterKey, name: "My user library" },
+    { dataURI: groupPath + "/items", apikey: masterKey, name: "My group library" }
+];
+
+const initRequests = analyzeUserRequests(defaultReqs);
+const initSettings = setupInitialSettings({});
+
+const init = {
+    ...initRequests,
+    ...initSettings
+};
 
 export const withUserSettings = (Story, context) => {
     const { parameters: { userSettings = {} } } = context;
 
-    if(userSettings){
-        return (
-            <UserSettings.Provider value={userSettings}>
-                <Story {...context} />
-            </UserSettings.Provider>
-        );
-    } else {
-        return <Story {...context} />;
-    }
+    return <UserSettingsProvider init={{ ...init, ...userSettings }}>
+        <Story {...context} />
+    </UserSettingsProvider>
 };
