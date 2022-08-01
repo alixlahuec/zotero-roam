01efe5c770aae098bc1a728ed694ca7907a91b7e
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-01 12:09:26 -0400

    chore: cleanup settings provider syntax

--- src/components/UserSettings/Provider.jsx
+++ src/components/UserSettings/Provider.jsx
@@ -6,18 +6,19 @@ import { SETTINGS_CONFIG } from "./mapping";
 import * as customPropTypes from "../../propTypes";
 
 // https://stackoverflow.com/questions/51504506/too-many-react-context-providers
-function UserSettingsProvider({ extensionAPI, children, init }){
+function UserSettingsProvider({ extensionAPI = {}, children, init }){
 	const updater = useCallback((id, val) => {
-		if(!extensionAPI){
+		if(!extensionAPI.settings){
 			return null;
 		} else {
+			console.log(extensionAPI, id, val);
 			extensionAPI.settings.set(id, val);
 		}
 	}, [extensionAPI]);
 
 	return SETTINGS_CONFIG.reduceRight((acc, provider) => {
 		const { component: Comp, id } = provider;
-		return <Comp id={id} init={init[id]} updater={(val) => updater(id, val)} >{acc}</Comp>;
+		return <Comp init={init[id]} updater={(val) => updater(id, val)} >{acc}</Comp>;
 	}, children);
 }
 UserSettingsProvider.propTypes = {
