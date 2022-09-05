0f4a4768899a2b77b66f49647af78e3067f23b5d
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-09-05 15:13:48 -0400

    create sandbox module

---
+++ sandbox.js
@@ -0,0 +1,128 @@
+/* istanbul ignore file */
+import { QueryClient } from "@tanstack/react-query";
+
+import { sampleAnnot, sampleImageAnnot } from "./mocks/zotero/annotations";
+import { apiKeys } from "./mocks/zotero/keys";
+import { findCollections } from "./mocks/zotero/collections";
+import { findItems } from "./mocks/zotero/items";
+import { libraries } from "./mocks/zotero/libraries";
+import { sampleNote } from "./mocks/zotero/notes";
+import { samplePDF } from "./mocks/zotero/pdfs";
+
+import ZoteroRoam from "./src/extension";
+import { initialize } from "./src/setup";
+
+
+const { keyWithFullAccess: masterKey } = apiKeys;
+const { userLibrary } = libraries;
+
+class ZoteroRoamSandbox extends ZoteroRoam {
+	constructor(){
+		const INSTALL_CONTEXT = "sandbox";
+
+		const { requests, settings } = initialize(INSTALL_CONTEXT, {
+			manualSettings: {
+				dataRequests: [
+					{ 
+						apikey: masterKey,
+						library: {
+							type: userLibrary.type,
+							id: userLibrary.id
+						}
+					}
+				],
+				metadata: {
+					func: "customFunction",
+					use: "function",
+				}
+			}
+		});
+
+		const queryClient = new QueryClient({
+			defaultOptions: {
+				queries: {
+					cacheTime: Infinity,
+					staleTime: Infinity
+				}
+			}
+		});
+
+		queryClient.setQueryData(
+			["items", { apikey: masterKey, dataURI: userLibrary.path + "/items", library: userLibrary.path }],
+			(_prev) => ({
+				data: [
+					...findItems({ type: userLibrary.type, id: userLibrary.id }),
+					sampleAnnot,
+					sampleImageAnnot,
+					sampleNote,
+					samplePDF
+				],
+				lastUpdated: userLibrary.version
+			})
+		);
+
+		queryClient.setQueryData(
+			["collections", { apikey: masterKey, library: userLibrary.path }],
+			(_prev) => ({
+				data: findCollections(userLibrary.type, userLibrary.id, 0),
+				lastUpdated: userLibrary.version
+			})
+		);
+
+		super({ queryClient, requests, settings });
+	}
+
+	updateLibraries(){
+		throw new Error("Updating the libraries used in the sandbox is not allowed.");
+	}
+
+	getBibEntries(){
+		throw new Error("Generating bibliographic information is not available in the sandbox.");
+	}
+
+	getItemCitation(){
+		throw new Error("Generating citation information is not available in the sandbox.");
+	}
+
+	getTags(){
+		throw new Error("Retrieving data on library tags is not allowed in the sandbox.");
+	}
+}
+
+function blockObjectToHTML(object){
+	let objectHTML = "";
+	
+	if(typeof(object.string) === "undefined"){
+		throw new Error("All blocks passed as an Object must have a string property");
+	} else {
+		objectHTML = objectHTML + `<li>${object.string}`;
+		
+		if(typeof(object.children) !== "undefined"){
+			if(object.children.length > 0){
+				objectHTML = objectHTML + arrayToHTML(object.children);
+			}
+		}
+		objectHTML = objectHTML + " </li>";
+	}
+	return objectHTML;
+}
+
+function arrayToHTML(arr){
+	let renderedHTML = "<ul>";
+	arr.forEach((el) => {
+		if(el.constructor === Object){
+			renderedHTML = renderedHTML + blockObjectToHTML(el);
+		} else if(el.constructor === String) {
+			renderedHTML = renderedHTML + `<li>${el} </li>`;
+		} else {
+			throw new Error("All array items should be of type String or Object");
+		}
+	});
+	renderedHTML = renderedHTML + "</ul>";
+	return renderedHTML;
+}
+
+export default {
+	arrayToHTML,
+	ZoteroRoamSandbox
+};
