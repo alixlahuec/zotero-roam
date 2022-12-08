84dad290fa5e56d38467fe997c696d209e6ecba2
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-12-07 20:23:21 -0500

    memoize useWriteableLibraries

--- src/api/queries.js
+++ src/api/queries.js
@@ -170,25 +170,28 @@ const useQuery_Tags = (libraries, opts = {}) => {
  * @returns {{data: ZoteroLibrary[], isLoading: Boolean}} The operation's status and outcome
  */
 const useWriteableLibraries = (libraries) => {
-	const apiKeys = Array.from(new Set(libraries.map(lib => lib.apikey)));
+	const apiKeys = useMemo(() => Array.from(new Set(libraries.map(lib => lib.apikey))), [libraries]);
 	const permissionQueries = useQuery_Permissions(apiKeys, {
 		notifyOnChangeProps: ["data", "isLoading"]
 	});
-	const isLoading = permissionQueries.some(q => q.isLoading);
-	const permissions = permissionQueries.map(q => q.data || []).flat(1);
 
-	const data = libraries
-		.filter(lib => {
-			const keyData = permissions.find(k => k.key == lib.apikey);
-			if(!keyData){
-				return false;
-			} else {
-				const { access } = keyData;
-				const [libType, libId] = lib.path.split("/");
-				const permissionsList = libType == "users" ? (access.user || {}) : (access.groups[libId] || access.groups.all);
-				return permissionsList?.write || false;
-			}
-		});
+	const isLoading = permissionQueries.some(q => q.isLoading);
+	const permissions = useMemo(() => permissionQueries.map(q => q.data || []).flat(1), [permissionQueries]);
+
+	const data = useMemo(() => {
+		return libraries
+			.filter(lib => {
+				const keyData = permissions.find(k => k.key == lib.apikey);
+				if (!keyData) {
+					return false;
+				} else {
+					const { access } = keyData;
+					const [libType, libId] = lib.path.split("/");
+					const permissionsList = libType == "users" ? (access.user || {}) : (access.groups[libId] || access.groups.all);
+					return permissionsList?.write || false;
+				}
+			});
+	}, [libraries, permissions]);
 	
 	return {
 		data,
