a2f0a151a69e9e69f0e9831d400f21470e603e14
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-12-12 20:49:58 -0500

    revert unrelated change

--- src/smartblocks.js
+++ src/smartblocks.js
@@ -243,7 +243,7 @@ function reformatImportableBlocks(arr){
 				};
 			} else {
 				console.log(blck);
-				throw new Error(`All array items should be of type String or Object, not ${blck.constructor.name}`);
+				throw new Error(`All array items should be of type String or Object, not ${blck.constructor}`);
 			}
 		});
 	}
