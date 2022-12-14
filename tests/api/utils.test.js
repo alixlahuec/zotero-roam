a9cb4fb5728f6ade0cc3016f455099ee5ac7f733
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-12-13 19:07:56 -0500

    update tests

--- tests/api/utils.test.js
+++ tests/api/utils.test.js
@@ -247,6 +247,8 @@ test("Selecting and formatting Semantic DOIs", () => {
 
 	expect(parseSemanticDOIs(testItems))
 		.toEqual([
+			{ doi: false },
+			{ doi: false },
 			{ doi: "10.1186/s40985-018-0094-7" },
 			{ doi: "10.1370/afm.1918" }
 		]);
