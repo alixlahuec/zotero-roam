edec15a33cfbdfe4e3616631a8ce49656d7d212f
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-02-07 22:31:35 -0500

    create IDB service

---
+++ src/services/IDBService.js
@@ -0,0 +1,41 @@
+import { openDB } from "idb";
+
+
+class IDBService {
+	/** @private */
+	#db;
+	/** @private */
+	#storeName;
+
+	/**
+	 * @param {{
+	 * dbName: String,
+	 * dbVersion: Number
+	 * }} context - The context in which the instance is being created
+	 */
+	constructor({ dbName = "ZOTEROROAM_REACT_QUERY_CACHE", dbVersion = 1, storeName = "keyval" }){
+		this.#storeName = storeName;
+		this.#db = openDB(dbName, dbVersion, {
+			upgrade: (database, _oldVersion, _newVersion, _transaction) => {
+				database.createObjectStore(this.#storeName);
+			}
+		});
+	}
+	async get(key){
+		return (await this.#db).get(this.#storeName, key);
+	}
+
+	async set(key, value) {
+		return (await this.#db).put(this.#storeName, value, key);
+	}
+
+	async delete(key) {
+		return (await this.#db).delete(this.#storeName, key);
+	}
+
+	async clear() {
+		return (await this.#db).clear(this.#storeName);
+	}
+}
+
+export default IDBService;
