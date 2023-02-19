8dde41f463685308f0c9369be321312d4da9e7d2
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-02-18 23:55:17 -0500

    simplify story to fix testing issue

--- src/components/ClearCacheButton/index.stories.jsx
+++ src/components/ClearCacheButton/index.stories.jsx
@@ -1,41 +1,20 @@
-import { useEffect } from "react";
 import ClearCacheButton from "Components/ClearCacheButton";
 
 
+window.zoteroRoam = {
+	isDataCached: () => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(false);
+			}, 800);
+		});
+	}
+};
+
 export default {
 	component: ClearCacheButton,
-	parameters: {
-		returnValue: false
-	},
-	decorators: [
-		(Story, context) => {
-			const { parameters: { returnValue } } = context;
-
-			useEffect(() => {
-				window.zoteroRoam = {
-					isDataCached: () => {
-						return new Promise((resolve) => {
-							setTimeout(() => {
-								resolve(returnValue);
-							}, 800);
-						});
-					}
-				};
-			}, [returnValue]);
-
-			return <Story />;
-		}
-	]
 };
 
 const Template = () => <ClearCacheButton />;
 
 export const NoData = Template.bind({});
-NoData.parameters = {
-	returnValue: false
-};
-
-export const WithData = Template.bind({});
-NoData.parameters = {
-	returnValue: true
-};
