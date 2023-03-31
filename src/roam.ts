ff762a27da609980e4d575bb9d0172e6f47247e7
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-03-31 08:44:54 -0400

    chore: convert Roam utils to TS (#243)

--- src/roam.ts
+++ src/roam.ts
@@ -3,13 +3,16 @@ import { executeFunctionByName } from "./utils";
 import { _formatNotes, _getItemMetadata } from "./extension";
 import { emitCustomEvent } from "./events";
 import { use_smartblock_metadata } from "./smartblocks";
+import { RCitekeyPages, RCitekeyPagesWithEditTime, RCursorLocation, RImportableBlock, RImportableElement, ZItemAnnotation, ZItemAttachment, ZItemNote, ZItemTop } from "Types/transforms";
+import { Roam } from "Types/externals";
+import { SettingsAnnotations, SettingsMetadata, SettingsNotes, SettingsTypemap } from "Types/extension";
 
 
 /** Adds Roam blocks to a parent UID based on an Object block template.
- * @param {String} parentUID - The UID of the parent (Roam block or page) 
- * @param {RImportableBlock} object - The block Object to use as template 
+ * @param parentUID - The UID of the parent (Roam block or page) 
+ * @param object - The block Object to use as template 
  */
-async function addBlockObject(parentUID, object, order = 0) {
+async function addBlockObject(parentUID: string, object: RImportableBlock, order: Roam.BlockOrder = 0) {
 	const { string: blockString, children = [], ...opts } = object;
 	
 	if(typeof(blockString) === "undefined"){
@@ -33,10 +36,10 @@ async function addBlockObject(parentUID, object, order = 0) {
 			// Go through each child element, starting by the last
 			// Recursion will ensure all nested children will be added
 			for(let j = children.length - 1; j >= 0; j--){
-				if(children[j].constructor === Object){
+				if(typeof(children[j]) === "object"){
 					// eslint-disable-next-line no-await-in-loop
 					await addBlockObject(blockUID, children[j], order);
-				} else if(children[j].constructor === String){
+				} else if(typeof(children[j]) === "string"){
 					// eslint-disable-next-line no-await-in-loop
 					await createRoamBlock(blockUID, children[j], order, {});
 				} else {
@@ -47,7 +50,7 @@ async function addBlockObject(parentUID, object, order = 0) {
 							element: children[j]
 						}
 					});
-					throw new Error(`All children array items should be of type String or Object, not ${children[j].constructor.name}`);
+					throw new Error(`All children array items should be of type String or Object, not ${typeof(children[j])}`);
 				}
 			}
 		} else {
@@ -58,17 +61,17 @@ async function addBlockObject(parentUID, object, order = 0) {
 					object
 				}
 			});
-			throw new Error(`If provided, the 'children' property of a block should be an Array, not ${children.constructor.name}`);
+			throw new Error(`If provided, the 'children' property of a block should be an Array, not ${typeof(children)}`);
 		}
 	}
 }
 
 /** Adds Roam blocks to a parent UID, based on an array input.
- * @param {String} parentUID - The UID of the parent (Roam block or page) 
- * @param {RImportableElement[]} arr - The array to use as template
+ * @param parentUID - The UID of the parent (Roam block or page) 
+ * @param arr - The array to use as template
  * @returns The outcome of the operation
  */
-async function addBlocksArray(parentUID, arr, order = 0){
+async function addBlocksArray(parentUID: string, arr: RImportableElement[], order: Roam.BlockOrder = 0){
 	const defaultOutcome = {
 		args: {
 			blocks: arr,
@@ -81,15 +84,16 @@ async function addBlocksArray(parentUID, arr, order = 0){
 	if(arr.length > 0){
 		try{
 			// Go through the array items in reverse order, so that each block gets added to the "same" position
-			for(let k = arr.length - 1; k >= 0; k--){
+			for (let k = arr.length - 1; k >= 0; k--){
+				const elem = arr[k];
 				// If the element is an Object, pass it to addBlockObject to recursively process its contents
-				if(arr[k].constructor === Object){
+				if(typeof(elem) === "object"){
 					// eslint-disable-next-line no-await-in-loop
-					await addBlockObject(parentUID, arr[k], order);
-				} else if(arr[k].constructor === String) {
+					await addBlockObject(parentUID, elem, order);
+				} else if(typeof(elem) === "string") {
 					// If the element is a simple String, add the corresponding block & move on
 					// eslint-disable-next-line no-await-in-loop
-					await createRoamBlock(parentUID, arr[k], order, {});
+					await createRoamBlock(parentUID, elem, order, {});
 				} else {
 					window.zoteroRoam?.error?.({
 						origin: "Metadata",
@@ -121,12 +125,11 @@ async function addBlocksArray(parentUID, arr, order = 0){
 }
 
 /** Adds an entry to Roam's Command Palette
- * @param {String} label - The label for the menu option 
- * @param {() => void} onSelect - The callback to execute upon selection
- * @param {Roam.ExtensionAPI?} extensionAPI - The API specifically available to the extension
- * @see https://roamresearch.com/#/app/developer-documentation/page/rAkidgrv3
+ * @param label - The label for the menu option 
+ * @param onSelect - The callback to execute upon selection
+ * @param extensionAPI - The API specifically available to the extension
  */
-function addPaletteCommand(label, onSelect, extensionAPI = {}) {
+function addPaletteCommand(label: string, onSelect: () => void, extensionAPI: (Roam.ExtensionAPI | Record<string, never>) = {}) {
 	const command = {
 		label,
 		callback: onSelect,
@@ -141,14 +144,13 @@ function addPaletteCommand(label, onSelect, extensionAPI = {}) {
 }
 
 /** Adds a single Roam block to a parent UID, with optional formatting.
- * @param {String} parentUID - The UID of the parent (Roam block or page)
- * @param {String} string - The text contents of the block
- * @param {Number} order - The order of the block on the page
- * @param {Object} opts - (optional) Additional formatting to be used (`heading`, `text-align`, ...). See the Roam Alpha API documentation for the complete list of available options.
+ * @param parentUID - The UID of the parent (Roam block or page)
+ * @param string - The text contents of the block
+ * @param order - The order of the block on the page
+ * @param opts - (optional) Additional formatting to be used (`heading`, `text-align`, ...). See the Roam Alpha API documentation for the complete list of available options.
  * @returns The UID of the created block
- * @see https://roamresearch.com/#/app/developer-documentation/page/Sq5IhwNQY
  */
-async function createRoamBlock(parentUID, string, order = 0, opts = {}) {
+async function createRoamBlock(parentUID: string, string: string, order: Roam.BlockOrder = 0, opts: Record<string, any> = {}) {
 	const blockUID = window.roamAlphaAPI.util.generateUID();
 	const blockContents = {
 		"string": string,
@@ -166,12 +168,15 @@ async function createRoamBlock(parentUID, string, order = 0, opts = {}) {
 }
 
 /** Searches for a Roam block by its contents, under a given parent
- * @param {String} string - The block's contents
- * @param {String} parentUID - The UID of the targeted parent (Roam block or page)
- * @returns {{uid: String, children?: Array}|false} The details of the Roam block (if it exists), otherwise `false`
+ * @returns The details of the Roam block (if it exists), otherwise `false`
 */
-function findRoamBlock(string, parentUID){
-	const blockSearch = window.roamAlphaAPI.q(`[
+function findRoamBlock(
+	/** The block's contents */
+	string: string,
+	/** The UID of the targeted parent (Roam block or page) */
+	parentUID: string
+) {
+	const blockSearch = window.roamAlphaAPI.q<[[{ uid: string, children?: any[] }]?]>(`[
 		:find (pull ?b [:block/uid :block/children])
 		:in $ ?string ?parentUID
 		:where
@@ -181,7 +186,7 @@ function findRoamBlock(string, parentUID){
 			[?b :block/string ?string]
 	]`, string, parentUID);
 
-	if(blockSearch.length > 0){
+	if(blockSearch[0]){
 		return blockSearch[0][0];
 	} else {
 		return false;
@@ -189,18 +194,20 @@ function findRoamBlock(string, parentUID){
 }
 
 /** Searches a Roam page by its title
- * @param {String} title - The title to be searched
- * @returns {String|false} The UID of the Roam page (if it exists), otherwise `false`
+ * @returns The UID of the Roam page (if it exists), otherwise `false`
  */
-function findRoamPage(title){
-	const pageSearch = window.roamAlphaAPI.q(`[
+function findRoamPage(
+	/** The title to be searched */
+	title: string
+) {
+	const pageSearch = window.roamAlphaAPI.q<[[string]?]>(`[
 		:find ?uid 
 		:in $ ?title 
 		:where
 			[?p :node/title ?title]
 			[?p :block/uid ?uid]
 		]`, title);
-	if(pageSearch.length > 0){
+	if(pageSearch[0]){
 		return pageSearch[0][0];
 	} else{
 		return false;
@@ -208,10 +215,10 @@ function findRoamPage(title){
 }
 
 /** Retrieves the full list of Roam pages, sorted in alphabetical order
- * @returns {String[]} The array of all page titles, sorted from A-Z
+ * @returns The array of all page titles, sorted from A-Z
  */
 function getAllPages(){
-	return window.roamAlphaAPI.q(`[
+	return window.roamAlphaAPI.q<[string][]>(`[
 		:find ?title 
 		:where
 			[?e :node/title ?title]
@@ -221,10 +228,10 @@ function getAllPages(){
 }
 
 /** Retrieves the list of citekey pages (i.e, starting with `@`) in the Roam graph
- * @returns {RCitekeyPages} A Map whose `keys` are the pages' titles, and whose `entries` are the pages' UIDs
+ * @returns A Map whose `keys` are the pages' titles, and whose `entries` are the pages' UIDs
  */
-function getCitekeyPages(){
-	return new Map(window.roamAlphaAPI.q(`[
+function getCitekeyPages(): RCitekeyPages{
+	return new Map(window.roamAlphaAPI.q<[string, string][]>(`[
 		:find ?title ?uid 
 		:where
 			[?e :node/title ?title]
@@ -233,11 +240,9 @@ function getCitekeyPages(){
 		]`));
 }
 
-/** Retrieves the list of citekey pages (i.e, starting with `@`) in the Roam graph, with their last-edit time
- * @returns {Map<String, {edited: Date, uid: String}>} A Map whose `keys` are the pages' titles, and whose `entries` contain the pages' UIDs and last-edit times
- */
-function getCitekeyPagesWithEditTime(){
-	return new Map(window.roamAlphaAPI.q(`[
+/** Retrieves the list of citekey pages (i.e, starting with `@`) in the Roam graph, with their last-edit time */
+function getCitekeyPagesWithEditTime(): RCitekeyPagesWithEditTime{
+	return new Map(window.roamAlphaAPI.q<{ title: string, time: number, uid: string, _parents: { time: number }[] }[]>(`[
 		:find [(pull ?e [:node/title :edit/time :block/uid {:block/_parents [:edit/time]}])...] 
 		:where
 			[?e :node/title ?t]
@@ -251,18 +256,16 @@ function getCitekeyPagesWithEditTime(){
 		}));
 }
 
-/** Retrieves the current cursor location in the Roam interface, to enable returning focus to its previous state after an interaction with the extension's interface (e.g opening a dialog).
- * @returns {RCursorLocation} Information about the cursor's location
- */
-function getCurrentCursorLocation(){
+/** Retrieves the current cursor location in the Roam interface, to enable returning focus to its previous state after an interaction with the extension's interface (e.g opening a dialog). */
+function getCurrentCursorLocation() {
 	const { "block-uid": blockUID, "window-id": windowID } = (window.roamAlphaAPI.ui.getFocusedBlock() || {});
 	if(!blockUID || !windowID){ return null; }
 
 	const blockElementID = ["block-input", windowID, blockUID].join("-");
-	const blockElement = document.getElementById(blockElementID) || {};
-	const { selectionStart = null, selectionEnd = null } = blockElement;
+	const blockElement = document.getElementById(blockElementID);
+	const { selectionStart = null, selectionEnd = null } = blockElement || {} as Record<string,any>;
 
-	const output = {
+	const output: RCursorLocation = {
 		id: blockElementID,
 		location: {
 			"block-uid": blockUID,
@@ -280,19 +283,19 @@ function getCurrentCursorLocation(){
 	return output;
 }
 
-/** Retrieve the name of the current Roam graph.
- * @returns {String} The name of the graph
- */
+/** Retrieve the name of the current Roam graph. */
 function getGraphName(){
 	return window.roamAlphaAPI.graph.name;
 }
 
-/** Retrieves the full list of Roam pages whose title begins with any of the keys provided
- * @param {String[]} keys - The Array of keys for which to retrieve Roam pages 
- * @returns {{title: String, uid: String}[]} The Array of pages whose title begins with any of the specified initials
+/** Retrieves the full list of Roam pages whose title begins with any of the keys provided 
+ * @returns The Array of pages whose title begins with any of the specified initials
  */
-function getInitialedPages(keys){
-	return window.roamAlphaAPI.q(`[
+function getInitialedPages(
+	/** The Array of keys for which to retrieve Roam pages */
+	keys: string[]
+) {
+	return window.roamAlphaAPI.q<{ title: string, uid: string }[][]>(`[
 		:find (pull ?e [:node/title :block/uid]) 
 		:in $ [?k ...] 
 		:where
@@ -304,24 +307,22 @@ function getInitialedPages(keys){
 
 /** Imports an item's metadata as Roam blocks
  * @fires zotero-roam:metadata-added
- * @param {{item: ZItemTop, pdfs?: ZItemAttachment[], notes?: (ZItemNote|ZItemAnnotation)[]}} itemData - The item's Zotero data and its children, if any
- * @param {String|false} uid - The UID of the item's Roam page (if it exists), otherwise a falsy value 
- * @param {SettingsMetadata} metadataSettings - The user's `metadata` settings 
- * @param {SettingsTypemap} typemap - The user's `typemap` settings
- * @param {SettingsNotes} notesSettings - The user's `notes` settings
- * @param {SettingsAnnotations} annotationsSettings - The user's `annotations` settings
  * @returns If successful, a detailed outcome of the import ; otherwise, the first error encountered.
  */
-async function importItemMetadata({ item, pdfs = [], notes = [] } = {}, uid, metadataSettings, typemap, notesSettings, annotationsSettings){
+async function importItemMetadata(
+	/** The item's Zotero data and its children, if any */
+	{ item, pdfs = [], notes = [] }: { item: ZItemTop, pdfs?: ZItemAttachment[], notes?: (ZItemNote | ZItemAnnotation)[] },
+	/** The UID of the item's Roam page (if it exists), otherwise `false` */
+	uid: string | false,
+	metadataSettings: SettingsMetadata, typemap: SettingsTypemap, notesSettings: SettingsNotes, annotationsSettings: SettingsAnnotations
+) {
 	const title = "@" + item.key;
 	const pageUID = uid || window.roamAlphaAPI.util.generateUID();
-	const page = { new: null, title, uid: pageUID };
+	const page = { new: false, title, uid: pageUID };
 	
 	if(pageUID != uid){
 		window.roamAlphaAPI.createPage({ page: { title, "uid": pageUID } });
 		page.new = true;
-	} else {
-		page.new = false;
 	}
 	
 	const { use = "default", func = "", smartblock: { param, paramValue } } = metadataSettings;
@@ -395,16 +396,19 @@ async function importItemMetadata({ item, pdfs = [], notes = [] } = {}, uid, met
 
 /** Imports an item's notes as Roam blocks
  * @fires zotero-roam:notes-added
- * @param {{item: ZoteroAPI.ItemTop, notes: (ZoteroAPI.ItemNote|ZoteroAPI.ItemAnnotation)[]}} itemData - The item's Zotero data and its notes, if any 
- * @param {String|false} uid - The UID of the item's Roam page (if it exists), otherwise a falsy value
- * @param {SettingsNotes} notesSettings - The user's `notes` settings
- * @param {SettingsAnnotations} annotationsSettings - The user's `annotations` settings
  * @returns If successful, a detailed outcome of the immport ; otherwise, the first error encountered.
  */
-async function importItemNotes({ item, notes = [] } = {}, uid, notesSettings, annotationsSettings){
+async function importItemNotes(
+	/** The item's Zotero data and its notes, if any */
+	{ item, notes = [] }: { item: ZItemTop, notes?: (ZItemNote | ZItemAnnotation)[] },
+	/** The UID of the item's Roam page (if it exists), otherwise `false` */
+	uid: string | false,
+	notesSettings: SettingsNotes,
+	annotationsSettings: SettingsAnnotations
+) {
 	const title = "@" + item.key;
 	const pageUID = uid || window.roamAlphaAPI.util.generateUID();
-	const page = { new: null, title, uid: pageUID };
+	const page: Record<string,unknown> = { new: null, title, uid: pageUID };
 
 	if(pageUID != uid){
 		window.roamAlphaAPI.createPage({ page: { title, "uid": pageUID } });
@@ -449,10 +453,8 @@ async function importItemNotes({ item, notes = [] } = {}, uid, notesSettings, an
 	}
 }
 
-/** Places the cursor in a given location, if it is specified
- * @param {RCursorLocation} place 
- */
-function maybeReturnCursorToPlace(place = {}){
+/** Places the cursor in a given location, if it is specified */
+function maybeReturnCursorToPlace(place: RCursorLocation | null){
 	if(place && place.location){
 		const { id, ...rest } = place;
 		const blockStillExists = document.getElementById(id) || false;
@@ -462,29 +464,23 @@ function maybeReturnCursorToPlace(place = {}){
 	}
 }
 
-/** Opens a Roam block or page in the right sidebar, based on its UID.
- * @param {String} uid - The UID of the Roam entity (block or page)
- * @param {String} type - The type of window that should be added to the sidebar. See the Roam Alpha API documentation for the complete list of available options.
- * @see https://roamresearch.com/#/app/developer-documentation/page/yHDobV8KV
- */
-async function openInSidebarByUID(uid, type = "outline"){
+/** Opens a Roam block or page in the right sidebar, based on its UID. */
+async function openInSidebarByUID(
+	/** The UID of the Roam entity (block or page) */
+	uid: string,
+	/** The type of window that should be added to the sidebar. */
+	type: Roam.SidebarWindowType = "outline"
+) {
 	await window.roamAlphaAPI.ui.rightSidebar.addWindow({ window: { type, "block-uid": uid } });
 }
 
-/** Navigates to a Roam page, based on its UID.
- * @param {String} uid - The UID of the Roam page 
- * @see https://roamresearch.com/#/app/developer-documentation/page/_VyuLpfWb
- */
-async function openPageByUID(uid){
+/** Navigates to a Roam page, based on its UID. */
+async function openPageByUID(uid: string){
 	await window.roamAlphaAPI.ui.mainWindow.openPage({ page: { uid } });
 }
 
-/** Removes an entry from Roam's Command Palette
- * @param {String} label - The label for the menu option
- * @param {Roam.ExtensionAPI?} extensionAPI - The API specifically available to the extension
- * @see https://roamresearch.com/#/app/developer-documentation/page/eG9ulEdWq
- */
-function removePaletteCommand(label, extensionAPI = {}) {
+/** Removes an entry from Roam's Command Palette */
+function removePaletteCommand(label: string, extensionAPI: Roam.ExtensionAPI | Record<string, never> = {}) {
 	const command = { label };
 	if (!extensionAPI.ui) {
 		window.roamAlphaAPI.ui.commandPalette.removeCommand(command);	
