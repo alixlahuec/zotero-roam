82bde1fff88a54f0761faa520a914f53ad378c19
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-04-20 19:10:17 -0400

    WebImport utils

--- src/components/GraphWatcher/WebImport/utils.ts
+++ src/components/GraphWatcher/WebImport/utils.ts
@@ -10,17 +10,17 @@ const findWebimportDivs = () => {
 const setWebimportDivs = (tags) => {
 	// Old blocks - are they still valid ?
 	findWebimportDivs()
-		.filter(b => !matchArrays(tags, JSON.parse(b.parentElement.getAttribute("data-page-links"))))
+		.filter(b => !matchArrays(tags, JSON.parse(b.parentElement?.getAttribute("data-page-links") || "[]")))
 		.forEach(b => b.remove());
 
 	// New blocks
 	const newTrigBlocks = Array.from(document.querySelectorAll(".rm-block:not(.rm-block--ghost)"))
-		.filter(b => !b.querySelector(`[class=${webimportClass}]`) && matchArrays(tags, JSON.parse(b.getAttribute("data-page-links"))));
+		.filter(b => !b.querySelector(`[class=${webimportClass}]`) && matchArrays(tags, JSON.parse(b.getAttribute("data-page-links") || "[]")));
     
 	const elem = document.createElement("span");
 	elem.classList.add(webimportClass);
 
-	newTrigBlocks.forEach(b => b.insertAdjacentElement("afterbegin", elem.cloneNode(true)));
+	newTrigBlocks.forEach(b => b.insertAdjacentElement("afterbegin", elem.cloneNode(true) as Element));
 };
 
 export { findWebimportDivs, setWebimportDivs };
