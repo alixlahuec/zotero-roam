c3737dc3190a959b560349c67cae5b16cd5ef8e6
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-05-16 20:00:26 -0400

    Item

--- src/components/NotesImport/Item/index.stories.tsx
+++ src/components/NotesImport/Item/index.stories.tsx
@@ -1,16 +1,19 @@
+import { ComponentProps } from "react";
 import { Classes } from "@blueprintjs/core";
+import { Meta, Story } from "@storybook/react";
 
 import NotesImportItem from "Components/NotesImport/Item";
 import { useBool } from "../../../hooks";
-
 import { sampleAnnot, sampleNote } from "Mocks";
 
 
+type Props = ComponentProps<typeof NotesImportItem>;
+
 export default {
 	component: NotesImportItem
-};
+} as Meta<Props>;
 
-const Template = (args) => {
+const Template: Story<Props> = (args) => {
 	const [isSelected, { toggle }] = useBool(args.isSelected || false);
 
 	return <ul className={Classes.LIST_UNSTYLED}>
