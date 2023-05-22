f28245f7d4e9b2f9ac8003baf224ee51342eb4bb
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-05-22 14:59:27 -0400

    update test-runner, a11y rules

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,61 +1,61 @@
 import * as fs from "fs";
 import { checkA11y, configureAxe,/* getViolations,*/ injectAxe } from "axe-playwright";
-import { Page } from "playwright-core";
-import { TestHook } from "@storybook/test-runner"
-
+import { AxeOptions } from "axe-playwright/dist/types";
+import { TestRunnerConfig } from "@storybook/test-runner";
 import { A11Y_RULES } from "./a11y-rules";
 
 
-// https://storybook.js.org/blog/automate-accessibility-tests-with-storybook/
-export const setup = () => {
-	fs.mkdir(
-		process.cwd() + "/__accessibility__/",
-		{ recursive: true },
-		(err) => {
-			if (err) { throw err; }
-		}
-	);
+const a11yConfig: TestRunnerConfig = {
+	// https://storybook.js.org/blog/automate-accessibility-tests-with-storybook/
+	setup() {
+		fs.mkdir(
+			process.cwd() + "/__accessibility__/",
+			{ recursive: true },
+			(err) => {
+				if (err) { throw err; }
+			}
+		);
+	},
+	async preRender(page, _context) {
+		await injectAxe(page);
+	},
+	async postRender(page, _context) {
+		await configureAxe(page, {
+			rules: A11Y_RULES
+		});
+
+		const config: AxeOptions = {
+			detailedReport: true,
+			detailedReportOptions: {
+				html: true
+			}
+		};
+
+		await checkA11y(page, "#storybook-root", config);
+
+		// To write violations to JSON files:
+		//
+		// const path = process.cwd() + `/__accessibility__/${context.id}.json`;
+
+		// if(fs.existsSync(path)){
+		//     fs.unlinkSync(path);
+		// }
+
+		// const violations = await getViolations(page, '#storybook-root', config);
+
+		// if(violations.length > 0) {
+		//     await new Promise((resolve, reject) => {
+		//         fs.writeFile(
+		//             path, 
+		//             JSON.stringify(violations, null, 2),
+		//             (err) => {
+		//                 if (err) reject(err);
+		//                 resolve();
+		//             }
+		//         );
+		//     });
+		// }
+	}
 };
 
-export const preRender: TestHook = async (page: Page, _context) => {
-	await injectAxe(page);
-};
-
-export const postRender: TestHook = async (page: Page, _context) => {
-
-	await configureAxe(page, {
-		rules: A11Y_RULES
-	});
-
-	const config = {
-		detailedReport: true,
-		detailedReportOptions: {
-			html: true
-		}
-	};
-
-	await checkA11y(page, "#storybook-root", config);
-
-	// To write violations to JSON files:
-	//
-	// const path = process.cwd() + `/__accessibility__/${context.id}.json`;
-
-	// if(fs.existsSync(path)){
-	//     fs.unlinkSync(path);
-	// }
-
-	// const violations = await getViolations(page, '#storybook-root', config);
-
-	// if(violations.length > 0) {
-	//     await new Promise((resolve, reject) => {
-	//         fs.writeFile(
-	//             path, 
-	//             JSON.stringify(violations, null, 2),
-	//             (err) => {
-	//                 if (err) reject(err);
-	//                 resolve();
-	//             }
-	//         );
-	//     });
-	// }
-};
+module.exports = a11yConfig;
