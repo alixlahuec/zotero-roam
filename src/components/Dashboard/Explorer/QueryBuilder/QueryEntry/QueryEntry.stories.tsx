886ddffe25420117340b8f09c75e5233f0acc57f
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-05-28 13:28:07 -0400

    QueryEntry - add interactions story

--- src/components/Dashboard/Explorer/QueryBuilder/QueryEntry/QueryEntry.stories.tsx
+++ src/components/Dashboard/Explorer/QueryBuilder/QueryEntry/QueryEntry.stories.tsx
@@ -1,6 +1,10 @@
 import { ComponentProps, useReducer } from "react";
 import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 import QueryEntry from "./QueryEntry";
+import { QueryProperty } from "../types";
 
 
 type Props = ComponentProps<typeof QueryEntry>;
@@ -43,5 +47,51 @@ export const MiddleChild: StoryObj<Props> = {
 		isFirstChild: false,
 		isOnlyChild: false,
 		useOR: false
+	}
+};
+
+export const WithInteractions: StoryObj<Props> = {
+	args: {
+		term: {
+			property: "Abstract",
+			relationship: "contains",
+			value: ""
+		}
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const frame = within(canvasElement.parentElement!);
+
+		const getPropertySelect = () => canvas.getByTitle("Select a property to query");
+		const getRelationshipSelect = () => canvas.getByTitle("Select a relationship to test for the property");
+
+		const selectProperty = async (property: QueryProperty) => {
+			await userEvent.click(getPropertySelect());
+			await userEvent.click(frame.getByText(property));
+		};
+
+		const selectRelationship = async (relationship) => {
+			await userEvent.click(getRelationshipSelect());
+			await userEvent.click(frame.getByText(relationship));
+		};
+
+		// Value change
+		await userEvent.type(canvas.getByPlaceholderText<HTMLInputElement>("Enter text"), "history");
+
+		// Property change - same input type
+		await selectProperty("Title");
+		await expect(canvas.getByPlaceholderText<HTMLInputElement>("Enter text").value).toBe("history");
+
+		// Property change - different input type, null
+		await selectProperty("DOI");
+		await expect(getRelationshipSelect().innerText).toBe("exists");
+
+		// Property change - different input type, text
+		await selectProperty("Abstract");
+		await expect(getRelationshipSelect().innerText).toBe("exists");
+
+		// Relationship change
+		await selectRelationship("contains");
+		await expect(canvas.getByPlaceholderText<HTMLInputElement>("Enter text").value).toBe("");
 	}
 };
