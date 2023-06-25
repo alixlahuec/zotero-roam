345f5b9847228a40bb9660b4c255d37644e82fc9
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-06-25 12:21:52 -0400

    fix dev-roam build config

--- dev/webpack.dev.roam.config.js
+++ dev/webpack.dev.roam.config.js
@@ -1,13 +1,14 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const { merge } = require("webpack-merge");
-const baseDevConfig = require("./webpack.dev.config");
 
-module.exports = merge(baseDevConfig, {
+// ! Do not merge with another config - it will give a SASS error (unrelated to the config contents, seems to be due to the merging process)
+module.exports = {
+	/* I/O */
+	context: path.resolve(__dirname, '../'),
+	entry: path.resolve("loader.tsx"),
 	experiments: {
 		outputModule: true,
 	},
-	entry: path.resolve("loader.tsx"),
 	output: {
 		path: path.resolve("."),
 		filename: "extension.js",
@@ -16,6 +17,30 @@ module.exports = merge(baseDevConfig, {
 			type: "module",
 		}
 	},
+
+	resolve: {
+		alias: {
+			"Mocks": path.resolve("mocks"),
+			"Roam": path.resolve("src", "roam.ts"),
+			"Components": path.resolve("src", "components"),
+			"Types": path.resolve("src", "types"),
+			"react-dom$": "react-dom/profiling",
+			"scheduler/tracing": "scheduler/tracing-profiling"
+		},
+		extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".sass"]
+	},
+
+	devtool: "source-map",
+	mode: "development",
+	optimization: {
+		minimize: false
+	},
+	performance: {
+		maxAssetSize: 2000000,
+		maxEntrypointSize: 2000000,
+	},
+
+	/* Plugins and loaders */
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: "extension.css",
@@ -55,4 +80,4 @@ module.exports = merge(baseDevConfig, {
 			},
 		],
 	}
-});
+};
