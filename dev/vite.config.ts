98aab84f28a1971deb2e199e033268678763018c
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2023-10-07 10:17:32 -0400

    add config, swap externals package

---
+++ dev/vite.config.ts
@@ -0,0 +1,93 @@
+import { resolve } from "path";
+import { defineConfig, AliasOptions, PluginOption } from "vite";
+import { BuildOptions } from "esbuild";
+
+import react from "@vitejs/plugin-react";
+import { viteExternalsPlugin } from "vite-plugin-externals";
+
+
+export default defineConfig(({ command, mode }) => {
+	let minify: BuildOptions["minify"] = true;
+	let sourcemap: BuildOptions["sourcemap"] = true;
+	let outDir: BuildOptions["outdir"] = "dist";
+	let rollupInput: Record<string, string>;
+	let extraAliases: AliasOptions = {};
+	let extraPlugins: PluginOption[] = [];
+
+	// Overrides for dev builds
+	if (command == "serve") {
+		minify = false;
+		sourcemap = false;
+		extraAliases = {
+			"react-dom$": "react-dom/profiling",
+			"scheduler/tracing": "scheduler/tracing-profiling"
+		}
+	}
+
+	// Mode-specific config
+	switch (mode) {
+		case "sandbox":
+			rollupInput = {
+				"zoteroRoam.sandbox": resolve("sandbox")
+			};
+			
+			extraAliases["Roam"] = resolve("mocks", "roam");
+
+			break;
+		case "roam":
+			outDir = ".";
+
+			rollupInput = {
+				"extension": resolve("loader")
+			};
+			
+			extraPlugins.push(viteExternalsPlugin({
+				"@blueprintjs/core": ["Blueprint", "Core"],
+				"@blueprintjs/datetime": ["Blueprint", "DateTime"],
+				"@blueprintjs/select": ["Blueprint", "Select"],
+				"idb": "idb",
+				"react": "React",
+				"react-dom": "ReactDOM",
+			}, { useWindow: true }));
+
+			break;
+		case "default":
+		default:
+			rollupInput = {
+				"zoteroRoam.min": resolve("src", "index")
+			};
+
+			break;
+	}
+
+	return {
+		build: {
+			target: "es2021",
+			minify,
+			sourcemap,
+			outDir,
+			emptyOutDir: false,
+			copyPublicDir: false, // needed because of msw
+			chunkSizeWarningLimit: 3000,
+			rollupOptions: {
+				input: rollupInput,
+				output: {
+					format: "esm",
+					assetFileNames: "extension.[ext]",
+					entryFileNames: "[name].js"
+				}
+			}
+		},
+		resolve: {
+			alias: {
+				"Mocks": resolve("mocks"),
+				"Roam": resolve("src", "roam"),
+				"Components": resolve("src", "components"),
+				"Styles": resolve("styles"),
+				"Types": resolve("src", "types"),
+				...extraAliases
+			}
+		},
+		plugins: [react(), ...extraPlugins]
+	};
+});
