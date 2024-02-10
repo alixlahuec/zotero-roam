36a1b237cc6466605182393cd6bc7d165a469731
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2024-02-10 16:55:59 -0500

    fix: update URLs for web links (#561)

--- src/utils.ts
+++ src/utils.ts
@@ -484,12 +484,16 @@ function getPDFLink(pdfItem: ZItemAttachment, as: ("href" | "markdown") = "href"
 }
 
 /** Creates a web link to a specific Zotero item, which opens in the browser.
+ * For user libraries: https://www.zotero.org/:username/items/:itemKey
+ * For group libraries: https://www.zotero.org/groups/:libraryId/:libraryName/items/:itemKey
  * @param item - The targeted Zotero item 
  * @param config - Additional settings 
  * @returns A link to the item, either as a Markdown link or a URL
  */
-function getWebLink(item: ZItemTop, { format = "markdown", text = "Web library" }: Partial<ZLinkOptions> = {}){
-	const location = ((item.library.type == "user") ? "users" : "groups") + `/${item.library.id}`;
+function getWebLink(item: ZItemTop, { format = "markdown", text = "Web library" }: Partial<ZLinkOptions> = {}) {
+	const location = item.library.type == "user"
+		? item.library.name
+		: `groups/${item.library.id}/${item.library.name}`;
 	const target = `https://www.zotero.org/${location}/items/${item.data.key}`;
 	switch(format){
 	case "markdown":
