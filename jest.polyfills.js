211d9d8d14a629cc68679d0605efa4e646bc4f9e
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2024-05-20 14:51:16 -0400

    add the other polyfills

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -10,3 +10,17 @@ Object.defineProperties(globalThis, {
 	TextDecoder: { value: TextDecoder },
 	TextEncoder: { value: TextEncoder }
 });
+
+import { Blob, File } from "buffer";
+import { fetch, Headers, FormData, Request, Response } from "undici";
+
+
+Object.defineProperties(globalThis, {
+	fetch: { value: fetch, writable: true },
+	Blob: { value: Blob },
+	File: { value: File },
+	Headers: { value: Headers },
+	FormData: { value: FormData },
+	Request: { value: Request },
+	Response: { value: Response }
+});
